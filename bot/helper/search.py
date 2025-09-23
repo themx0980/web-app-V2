d272d943c36f33c5c2d10926bffc550f420847b8
ishwar0980 <138602811+ishwar0980@users.noreply.github.com>
2025-09-24 03:23:29 +0530

    Update search.py

--- bot/helper/search.py
+++ bot/helper/search.py
@@ -1,9 +1,11 @@
 import re
 from bot.config import Telegram
 from bot.helper.database import Database
+from bot.helper.index import clean_title
 from bot.telegram import UserBot
 from os.path import splitext
 from bot.helper.file_size import get_readable_file_size
+
 
 db = Database()
 async def search(chat_id, query, page):
@@ -17,6 +19,7 @@ async def search(chat_id, query, page):
         title = file.file_name or post.caption or file.file_id
         title, _ = splitext(title)
         title = re.sub(r'[.,|_\',]', ' ', title)
+        title = clean_title(title)
         posts.append({"msg_id": post.id, "title": title,
                      "hash": file.file_unique_id[:6], "size": get_readable_file_size(file.file_size), "type": file.mime_type})
     return posts
